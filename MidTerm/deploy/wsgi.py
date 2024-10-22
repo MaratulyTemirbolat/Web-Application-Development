0533c9732226258f8e9d719624b5357d2bbd9de2
Temirbolat Maratuly <snake2009003@mail.ru>
2024-10-22 23:51:17 +0500

    [MidTerm] Update deploy/asgi.py and deploy/wsgi.py

--- MidTerm/deploy/wsgi.py
+++ MidTerm/deploy/wsgi.py
@@ -2,6 +2,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanagement.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'settings.env.local'
+)
 
 application = get_wsgi_application()
